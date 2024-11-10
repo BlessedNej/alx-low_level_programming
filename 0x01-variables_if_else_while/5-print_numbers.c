608df226a84d7335437e41c3b4fc252a076e00e4
BlessedNej <nejoblessing72@gmail.com>
2024-11-10 07:41:44 -0800

    prints all single decimals

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: prints all single decimals
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+		printf("%d", a++);
+
+	printf("\n");
+	return (0);
+}
