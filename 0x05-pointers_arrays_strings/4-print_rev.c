c9ed0961fb86ed5a4f306759678cb2acbb1fab49
BlessedNej <nejoblessing72@gmail.com>
2024-12-27 09:21:41 -0800

    prints in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,17 +9,20 @@
 
 void print_rev(char *s)
 {
-	int len = 0;
-	int i;
+	int i, j, len;
 
-	while (s[len] != '\0')
+	i = 0;
+
+	while (s[i] != '\0')
 	{
-		len++;
+		i++;
 	}
 
-	for (i = len; s[i] >= 0; i--)
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
 	{
-		_putchar(s[i]);
+		_putchar(s[j]);
 	}
 	_putchar('\n');
 }
