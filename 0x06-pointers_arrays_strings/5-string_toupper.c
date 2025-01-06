992511c76fad845477f2f361f496f5aa6cf57bc7
BlessedNej <nejoblessing72@gmail.com>
2025-01-06 10:19:47 -0800

    converts lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,9 +11,12 @@ char *string_toupper(char *c)
 {
 	int i;
 
-	for (i = 0; c[i] > 96 && c[i] < 123; i++)
+	for (i = 0; c[i] != '\0'; i++)
 	{
-		c[i] -= 32;
+		if (c[i] > 96 && c[i] < 123)
+		{
+			c[i] -= 32;
+		}
 	}
 
 	return (c);
