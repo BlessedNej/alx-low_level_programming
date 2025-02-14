760b3e6346a3cb8ba09498c406659fad4c9be5da
BlessedNej <nejoblessing72@gmail.com>
2025-02-14 13:52:35 -0800

    structs and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: No description for this struct
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_b: typedef for struct dog
+ */
+
+typedef struct dog dog_b;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif 
