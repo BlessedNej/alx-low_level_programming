7fe3d3bb88b2b56518f9a44979a26091ccc815f5
BlessedNej <nejoblessing72@gmail.com>
2025-05-05 12:01:00 -0700

    find the perimeter of an island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,7 @@ a function def island_perimeter(grid): that returns
 the perimeter of the island described in grid:
 """
 
+
 def island_perimeter(grid):
     """ returns the perimeter of the island discussed in grid"""
     height = len(grid)
